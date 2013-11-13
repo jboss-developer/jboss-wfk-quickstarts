a81ac87b8ae6582e8c101c21124eda210d250667
Paul Gier <pgier@redhat.com>
2013-11-12 21:29:20 -0600

    Add license info and update formatting

--- resteasy-spring/src/main/java/org/jboss/resteasy/test/smoke/SimpleResource.java
+++ resteasy-spring/src/main/java/org/jboss/resteasy/test/smoke/SimpleResource.java
@@ -1,3 +1,19 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.resteasy.test.smoke;
 
 import javax.ws.rs.Consumes;
@@ -11,51 +27,49 @@ import javax.ws.rs.QueryParam;
 
 /**
  * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
- * @version $Revision: 1 $
  */
 @Path("/")
 public class SimpleResource
 {
-   @GET
-   @Path("basic")
-   @Produces("text/plain")
-   public String getBasic()
-   {
-      System.out.println("getBasic()");
-      return "basic";
-   }
+    @GET
+    @Path("basic")
+    @Produces("text/plain")
+    public String getBasic()
+    {
+        System.out.println("getBasic()");
+        return "basic";
+    }
 
-   @PUT
-   @Path("basic")
-   @Consumes("text/plain")
-   public void putBasic(String body)
-   {
-      System.out.println(body);
-   }
+    @PUT
+    @Path("basic")
+    @Consumes("text/plain")
+    public void putBasic(String body)
+    {
+        System.out.println(body);
+    }
 
-   @GET
-   @Path("queryParam")
-   @Produces("text/plain")
-   public String getQueryParam(@QueryParam("param")String param)
-   {
-      return param;
-   }
+    @GET
+    @Path("queryParam")
+    @Produces("text/plain")
+    public String getQueryParam(@QueryParam("param") String param)
+    {
+        return param;
+    }
 
-   @GET
-   @Path("matrixParam")
-   @Produces("text/plain")
-   public String getMatrixParam(@MatrixParam("param")String param)
-   {
-      return param;
-   }
+    @GET
+    @Path("matrixParam")
+    @Produces("text/plain")
+    public String getMatrixParam(@MatrixParam("param") String param)
+    {
+        return param;
+    }
 
-   @GET
-   @Path("uriParam/{param}")
-   @Produces("text/plain")
-   public int getUriParam(@PathParam("param")int param)
-   {
-      return param;
-   }
-
+    @GET
+    @Path("uriParam/{param}")
+    @Produces("text/plain")
+    public int getUriParam(@PathParam("param") int param)
+    {
+        return param;
+    }
 
 }
--- resteasy-spring/src/test/java/org/jboss/resteasy/test/jboss/SmokeTest.java
+++ resteasy-spring/src/test/java/org/jboss/resteasy/test/jboss/SmokeTest.java
@@ -1,3 +1,19 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.resteasy.test.jboss;
 
 import org.apache.commons.httpclient.HttpClient;
@@ -9,93 +25,88 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.jboss.resteasy.util.HttpResponseCodes;
 
-
 /**
  * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
- * @version $Revision: 1 $
  */
 public class SmokeTest
 {
-   @Test
-   public void testNoDefaultsResource() throws Exception
-   {
-      HttpClient client = new HttpClient();
+    @Test
+    public void testNoDefaultsResource() throws Exception
+    {
+        HttpClient client = new HttpClient();
 
-      {
-         GetMethod method = new GetMethod("http://localhost:8080/spring-integration-test/basic");
-         int status = client.executeMethod(method);
-         Assert.assertEquals(HttpResponseCodes.SC_OK, status);
-         Assert.assertEquals("basic", method.getResponseBodyAsString());
-         method.releaseConnection();
-      }
-      {
-         PutMethod method = new PutMethod("http://localhost:8080/spring-integration-test/basic");
-         method.setRequestEntity(new StringRequestEntity("basic", "text/plain", null));
-         int status = client.executeMethod(method);
-         Assert.assertEquals(204, status);
-         method.releaseConnection();
-      }
-      {
-         GetMethod method = new GetMethod("http://localhost:8080/spring-integration-test/queryParam");
-         NameValuePair[] params = {new NameValuePair("param", "hello world")};
-         method.setQueryString(params);
-         int status = client.executeMethod(method);
-         Assert.assertEquals(HttpResponseCodes.SC_OK, status);
-         Assert.assertEquals("hello world", method.getResponseBodyAsString());
-         method.releaseConnection();
-      }
-      {
-         GetMethod method = new GetMethod("http://localhost:8080/spring-integration-test/uriParam/1234");
-         int status = client.executeMethod(method);
-         Assert.assertEquals(HttpResponseCodes.SC_OK, status);
-         Assert.assertEquals("1234", method.getResponseBodyAsString());
-         method.releaseConnection();
-      }
-   }
+        {
+            GetMethod method = new GetMethod("http://localhost:8080/spring-integration-test/basic");
+            int status = client.executeMethod(method);
+            Assert.assertEquals(HttpResponseCodes.SC_OK, status);
+            Assert.assertEquals("basic", method.getResponseBodyAsString());
+            method.releaseConnection();
+        }
+        {
+            PutMethod method = new PutMethod("http://localhost:8080/spring-integration-test/basic");
+            method.setRequestEntity(new StringRequestEntity("basic", "text/plain", null));
+            int status = client.executeMethod(method);
+            Assert.assertEquals(204, status);
+            method.releaseConnection();
+        }
+        {
+            GetMethod method = new GetMethod("http://localhost:8080/spring-integration-test/queryParam");
+            NameValuePair[] params = { new NameValuePair("param", "hello world") };
+            method.setQueryString(params);
+            int status = client.executeMethod(method);
+            Assert.assertEquals(HttpResponseCodes.SC_OK, status);
+            Assert.assertEquals("hello world", method.getResponseBodyAsString());
+            method.releaseConnection();
+        }
+        {
+            GetMethod method = new GetMethod("http://localhost:8080/spring-integration-test/uriParam/1234");
+            int status = client.executeMethod(method);
+            Assert.assertEquals(HttpResponseCodes.SC_OK, status);
+            Assert.assertEquals("1234", method.getResponseBodyAsString());
+            method.releaseConnection();
+        }
+    }
 
-   @Test
-   public void testLocatingResource() throws Exception
-   {
-      HttpClient client = new HttpClient();
+    @Test
+    public void testLocatingResource() throws Exception
+    {
+        HttpClient client = new HttpClient();
 
-      {
-         GetMethod method = new GetMethod("http://localhost:8080/spring-integration-test/locating/basic");
-         int status = client.executeMethod(method);
-         Assert.assertEquals(HttpResponseCodes.SC_OK, status);
-         Assert.assertEquals("basic", method.getResponseBodyAsString());
-         method.releaseConnection();
-      }
-      {
-         PutMethod method = new PutMethod("http://localhost:8080/spring-integration-test/locating/basic");
-         method.setRequestEntity(new StringRequestEntity("basic", "text/plain", null));
-         int status = client.executeMethod(method);
-         Assert.assertEquals(204, status);
-         method.releaseConnection();
-      }
-      {
-         GetMethod method = new GetMethod("http://localhost:8080/spring-integration-test/locating/queryParam");
-         NameValuePair[] params = {new NameValuePair("param", "hello world")};
-         method.setQueryString(params);
-         int status = client.executeMethod(method);
-         Assert.assertEquals(HttpResponseCodes.SC_OK, status);
-         Assert.assertEquals("hello world", method.getResponseBodyAsString());
-         method.releaseConnection();
-      }
-      {
-         GetMethod method = new GetMethod("http://localhost:8080/spring-integration-test/locating/uriParam/1234");
-         int status = client.executeMethod(method);
-         Assert.assertEquals(HttpResponseCodes.SC_OK, status);
-         Assert.assertEquals("1234", method.getResponseBodyAsString());
-         method.releaseConnection();
-      }
-   }
+        {
+            GetMethod method = new GetMethod("http://localhost:8080/spring-integration-test/locating/basic");
+            int status = client.executeMethod(method);
+            Assert.assertEquals(HttpResponseCodes.SC_OK, status);
+            Assert.assertEquals("basic", method.getResponseBodyAsString());
+            method.releaseConnection();
+        }
+        {
+            PutMethod method = new PutMethod("http://localhost:8080/spring-integration-test/locating/basic");
+            method.setRequestEntity(new StringRequestEntity("basic", "text/plain", null));
+            int status = client.executeMethod(method);
+            Assert.assertEquals(204, status);
+            method.releaseConnection();
+        }
+        {
+            GetMethod method = new GetMethod("http://localhost:8080/spring-integration-test/locating/queryParam");
+            NameValuePair[] params = { new NameValuePair("param", "hello world") };
+            method.setQueryString(params);
+            int status = client.executeMethod(method);
+            Assert.assertEquals(HttpResponseCodes.SC_OK, status);
+            Assert.assertEquals("hello world", method.getResponseBodyAsString());
+            method.releaseConnection();
+        }
+        {
+            GetMethod method = new GetMethod("http://localhost:8080/spring-integration-test/locating/uriParam/1234");
+            int status = client.executeMethod(method);
+            Assert.assertEquals(HttpResponseCodes.SC_OK, status);
+            Assert.assertEquals("1234", method.getResponseBodyAsString());
+            method.releaseConnection();
+        }
+    }
 
-   /*
-   public static Test suite() throws Exception
-   {
-      System.out.println("***********");
-      System.out.println(System.getProperty("jbosstest.deploy.dir"));
-      return getDeploySetup(SmokeTest.class, "spring-integration-test.war");
-   }
-   */
+    /*
+     * public static Test suite() throws Exception { System.out.println("***********");
+     * System.out.println(System.getProperty("jbosstest.deploy.dir")); return getDeploySetup(SmokeTest.class,
+     * "spring-integration-test.war"); }
+     */
 }
