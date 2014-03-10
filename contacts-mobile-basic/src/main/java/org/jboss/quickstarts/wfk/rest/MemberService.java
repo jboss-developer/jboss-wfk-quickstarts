2999f0818b1679a0f8b479b408040411e2f26f4c
ecervena <ecervena@redhat.com>
2014-03-10 09:18:28 +0100

    WFK2-537: Unused imports removed from contacts-mobile-basic

--- contacts-mobile-basic/src/main/java/org/jboss/quickstarts/wfk/rest/MemberService.java
+++ contacts-mobile-basic/src/main/java/org/jboss/quickstarts/wfk/rest/MemberService.java
@@ -16,16 +16,11 @@
  */
 package org.jboss.quickstarts.wfk.rest;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.TimeZone;
 import java.util.logging.Logger;
 
 import javax.ejb.Stateful;
@@ -44,7 +39,6 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
