ea7f8f060e2452a8c968a3d973633735b12ef77d
Vineet Reynolds <Vineet.Reynolds@gmail.com>
2014-06-06 21:09:11 +0530

    WFK2-314 Update QUnit to 1.14
    Also updated references to JavaScript libs in the
    contacts-mobile-basic test.

--- kitchensink-backbone/src/test/qunit/test/test.js
+++ kitchensink-backbone/src/test/qunit/test/test.js
@@ -18,34 +18,218 @@
 Unit tests that cover basic functionality of app.js.
  */
 
-module('Member Row Construction');
+var server;
 
-test('Build 2 Member Rows', function() {
+module('Member display tests', {
+    setup: function() {
+        server = sinon.fakeServer.create();
+        $("#qunit-fixture").empty();
+    },
+    teardown: function() {
+        server.restore();
+    }
+});
+
+test('Render a single member', function() {
     expect(1);
 
     var members = [{"email": "jane.doe@company.com", "id": 1, "name": "Jane Doe", "phoneNumber": "12312312311"},{"email": "john.doe@company.com", "id": 0, "name": "John Doe", "phoneNumber": "2125551212"}];
 
-    var html = buildMemberRows(members);
+    var member = new Member({"email": "jane.doe@company.com", "id": 1, "name": "Jane Doe", "phoneNumber": "12312312311"});
 
-    ok($(html).length == 2, 'Number of rows built: ' + length);
+    var view = new MemberView({model : member});
+
+    // Render the view
+    view.render();
+
+    var content = view.$el;
+    ok(content.length == 1, 'Number of rows built: ' + content.length);
 });
 
-test('Build 0 member Rows', function() {
+test('Render multiple members', function() {
     expect(1);
 
-    var members = [];
+    $("#qunit-fixture").append("<table id='member-table'><tbody id='members'></tbody></table>");
 
-    var html = buildMemberRows(members);
+    // Set up the rules for the fake server
+    server.respondWith("GET", /rest\/members(.*)/,
+        [200, { "Content-Type": "application/json" },
+            '[{"email": "jane.doe@company.com", "id": 1, "name": "Jane Doe", "phoneNumber": "12312312311"},{"email": "john.doe@company.com", "id": 0, "name": "John Doe", "phoneNumber": "2125551212"}]']);
 
-    ok($(html).length == 0, 'Created no rows for empty members');
+    var view = new ListAllMembersView({ el: "#qunit-fixture"});
+
+    // Render the view
+    view.render();
+
+    // Fake the server's response
+    server.respond();
+
+    var rows = view.$el.find("tr");
+    ok(rows.length == 2, 'Number of rows built: ' + rows.length);
 });
 
-module('Member Restful Calls');
+module('Tests for Backbone View interaction with RESTful backend', {
+    setup: function() {
+        server = sinon.fakeServer.create();
+        $("#qunit-fixture").empty();
+    },
+    teardown: function() {
+        server.restore();
+    }
+});
 
 test('Register a new member', function() {
-    ok(1==1,"TODO");
+    expect(3);
+
+    // Add the registration form for this test
+    var regForm = $("#register-form").html();
+    $("#qunit-fixture").html(regForm);
+
+    var view = new RegisterMemberView({ el: "#qunit-fixture"});
+
+    // Set up the rules for the fake server
+    server.respondWith("POST", /rest\/members(.*)/,
+        [200, { "Content-Type": "application/json" },
+            '{"email": "jane.doe@company.com", "id": 1, "name": "Jane Doe", "phoneNumber": "12312312311"}']);
+
+    // Render the view
+    view.render();
+
+    // Populate the registration form
+    $("#name").val("Jane Doe");
+    $("#email").val("jane.doe@company.com");
+    $("#phoneNumber").val("12312312311");
+
+    // Submit the form
+    $("#register").click();
+
+    // Fake the server's response
+    server.respond();
+
+    // Verify that a success message is displayed, since the server responded with a HTTP 200.
+    var msgs = $("span.success");
+    var errors = $("span.invalid");
+    ok(errors.length == 0, 'Number of errors: ' + errors.length);
+    ok(msgs.length == 1, 'Number of messages: ' + msgs.length);
+    equal(msgs.text(), "Member Registered", "The message should indicate that the member was registered");
 });
 
-test('Register a member with a duplicate email', function() {
-    ok(1==1,"TODO");
+test('Register a member with a duplicate email in the RegisterMemberView', function() {
+    expect(3);
+
+    // Add the registration form for this test
+    var regForm = $("#register-form").html();
+    $("#qunit-fixture").html(regForm);
+
+    var view = new RegisterMemberView({ el: "#qunit-fixture"});
+
+    // Set up the rules for the fake server
+    server.respondWith("POST", /rest\/members(.*)/,
+        [400, { "Content-Type": "application/json" },
+            '{"email":"Email taken"}']);
+
+    // Render the view
+    view.render();
+
+    // Populate the registration form
+    $("#name").val("Jane Doe");
+    $("#email").val("jane.doe@company.com");
+    $("#phoneNumber").val("12312312311");
+
+    // Submit the form
+    $("#reg").submit();
+
+    // Fake the server's response
+    server.respond();
+
+    // Verify that the error in the server response is displayed, since the server responded with a HTTP 400.
+    var msgs = $("span.success");
+    var errors = $("span.invalid");
+    ok(msgs.length == 0, 'Number of messages: ' + msgs.length);
+    ok(errors.length == 1, 'Number of errors: ' + errors.length);
+    equal(errors.text(), "Email taken", "The message contain the server's response informing the user about the duplicate email.");
+});
+
+module('Validation Tests', {
+    setup: function() {
+        $("#qunit-fixture").empty();
+    }
+});
+
+test('Attempt to register a member with an invalid name', function() {
+    expect(2);
+
+    // Add the registration form for this test
+    var regForm = $("#register-form").html();
+    $("#qunit-fixture").html(regForm);
+
+    var view = new RegisterMemberView({ el: "#qunit-fixture"});
+
+    // Render the view
+    view.render();
+
+    // Populate the registration form. Leave the name field blank
+    $("#name").val("");
+
+    // Submit the form
+    $("#reg").submit();
+
+    // Verify that a HTML5 validation error is displayed.
+    var msgs = $("span.success");
+    var errors = $("#name + span.invalid");
+    ok(msgs.length == 0, 'Number of messages: ' + msgs.length);
+    ok(errors.length == 1, 'Number of errors: ' + errors.length);
+});
+
+test('Attempt to register a member with an invalid email', function() {
+    expect(2);
+
+    // Add the registration form for this test
+    var regForm = $("#register-form").html();
+    $("#qunit-fixture").html(regForm);
+
+    var view = new RegisterMemberView({ el: "#qunit-fixture"});
+
+    // Render the view
+    view.render();
+
+    // Populate the registration form. Leave the name field blank
+    $("#name").val("Jane Doe");
+    $("#email").val("Jane Doe");
+
+    // Submit the form
+    $("#reg").submit();
+
+    // Verify that a HTML5 validation error is displayed.
+    var msgs = $("span.success");
+    var errors = $("#email + span.invalid");
+    ok(msgs.length == 0, 'Number of messages: ' + msgs.length);
+    ok(errors.length == 1, 'Number of errors: ' + errors.length);
+});
+
+test('Attempt to register a member with an invalid phone number', function() {
+    expect(2);
+
+    // Add the registration form for this test
+    var regForm = $("#register-form").html();
+    $("#qunit-fixture").html(regForm);
+
+    var view = new RegisterMemberView({ el: "#qunit-fixture"});
+
+    // Render the view
+    view.render();
+
+    // Populate the registration form. Leave the name field blank
+    $("#name").val("Jane Doe");
+    $("#email").val("jane.doe@company.com");
+    $("#phoneNumber").val("00");
+
+    // Submit the form
+    $("#reg").submit();
+
+    // Verify that a HTML5 validation error is displayed.
+    var msgs = $("span.success");
+    var errors = $("#phoneNumber + span.invalid");
+    ok(msgs.length == 0, 'Number of messages: ' + msgs.length);
+    ok(errors.length == 1, 'Number of errors: ' + errors.length);
 });
