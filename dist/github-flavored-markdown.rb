e488a678bed5ba96096e2290c2016d14d1eb5db9
Sande Gilda <sgilda@redhat.com>
2014-10-21 16:39:44 -0400

    JDF-783: Remove local CONTRIBUTING file and stop generating HTML for CONTRIBUTING.md files

--- dist/github-flavored-markdown.rb
+++ dist/github-flavored-markdown.rb
@@ -141,8 +141,8 @@ def markdown(source_path)
   text.gsub!("\[TOC\]", toc)
   rendered = markdown.render(text)
   metadata(source_path.path, rendered)
-  rendered = rendered.gsub(/README.md/, "README.html").gsub(/CONTRIBUTING.md/, "CONTRIBUTING.html")
-  '<!DOCTYPE html><html><head><title>README</title><link href="http://www.jboss.org/jdf/stylesheets/documentation.css" rel="stylesheet"></link><link href="http://www.jboss.org/jdf/stylesheets/pygments.css" rel="stylesheet"></link></head><body>' + rendered + '</body></html>'
+  rendered = rendered.gsub(/README.md/, "README.html")
+  '<!DOCTYPE html><html><head><title>README</title><link href="http://static.jboss.org/ffe/0/www/vendor/redhat/documentation.css" rel="stylesheet"></link><link href="http://static.jboss.org/ffe/0/www/vendor/redhat/pygments.css" rel="stylesheet"></link></head><body>' + rendered + '</body></html>'
   end
 
 def optionize(options)
